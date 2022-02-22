962ef35268a46c2eaa888a134e80cc896415c9a4
Adam <mojtaba.sadeghi1@gmail.com>
2022-02-21 18:58:26 -0800

    init

---
+++ src/main/java/com/readDatabase/ReadFile/ReadFileApplication.java
@@ -0,0 +1,13 @@
+package com.readDatabase.ReadFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readDatabase/ReadFile/ReadFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readDatabase.ReadFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
